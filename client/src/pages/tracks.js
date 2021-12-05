4b770dce8393dc160adb805afabbfad99eb3a750
Hans Y <hcy.dev@gmail.com>
2021-12-06 00:47:54 +1100

    complete part 1 - remove final folder

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import { QueryResult } from '../components';
+export const TRACKS = gql`
+  query HomeTracksQuery {
+    tracksForHome {
+      id
+      title
+      author {
+        photo
+        name
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, loading, error } = useQuery(TRACKS)
+
+  if (loading) return 'Loading...'
+  if (error) return `ERROR: ${error.message}`
+
+  return <Layout grid>
+    <QueryResult loading={loading} data={data} error={error}>
+      {
+        data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))
+      }
+    </QueryResult>
+  </Layout>
 };
 
 export default Tracks;
