e2e0d9b33e0bf794bd453970452ae1ef2d68883c
Hans Y <hcy.dev@gmail.com>
2021-12-12 23:40:11 +1100

    complete part 3

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,10 +1,21 @@
 const resolvers = {
     Query: {
         // returns an array of Tracks that will be used to populate
-        tracksFromHome: (_, __, { dataSources }) => dataSources.trackAPI.getTracksForHome()
+        tracksFromHome: (_, __, { dataSources }) =>
+            dataSources.trackAPI.getTracksForHome(),
+        // get a single track id, for the track page
+        track: (_, { id }, { dataSources }) =>
+            dataSources.trackAPI.getTrack(id)
     },
     Track: {
-        author: (parent, _, { dataSources }) => dataSources.trackAPI.getAuthor(parent.authorId)
+        author: (parent, _, { dataSources }) =>
+            dataSources.trackAPI.getAuthor(parent.authorId),
+        modules: (parent, _, { dataSources }) =>
+            dataSources.trackAPI.getTrackModules(parent.id)
+
+    },
+    Module: {
+
     }
 }
 
--- server/src/schema.js
+++ server/src/schema.js
@@ -2,8 +2,12 @@ const { gql } = require('apollo-server')
 
 
 const typeDefs = gql`
+
     type Query {
+        "return list of non-nullable tracks from the homepage grid"
         tracksFromHome: [Track!]!
+        "return a single track"
+        track(id: ID!): Track
     }
 
     "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +23,21 @@ const typeDefs = gql`
         length: Int
         "The number of modules this track contains"
         modulesCount: Int
+        "The track's complete description, can be in markdown format"
+        description: String
+        "the number of times a track has been viewed"
+        numberOfViews: Int
+        "The track's complete array of Modules"
+        modules: [Module!]!
+    }
+
+    "A module is a single unit of teaching. Multiple Modules compose a Track"
+    type Module {
+        id: ID!
+        "The tile of the module"
+        title: String!
+        "The lenght of module in minutes"
+        length: Int
     }
 
     "Author of a complete Track or a Module"
